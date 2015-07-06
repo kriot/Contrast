74e556eddf215afe502105bca9d8673927a828ef
Georgy Borisenko <gosha-bor@yandex.ru>
2015-07-06 18:04:30 +0300

    Hist tested, work fine

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include <set>
 #include <opencv2/opencv.hpp>
 
-const int COUNT_OF_THE_BIGGEST = 50;
+
+const int distance = 3;
+const double diff_factor = 0.001;
 
 std::vector<long long> calcHist(const cv::Mat& img, int ch)
 {
@@ -12,8 +14,7 @@ std::vector<long long> calcHist(const cv::Mat& img, int ch)
 	for(int i = 0; i < img.rows - 1; ++i) //Awful thing
 		for(int j = 0; j < img.cols; ++j)
 		{
-			auto t = img.at<cv::Vec4b>(i, j); 
-			res[img.at<cv::Vec4b>(i, j)[ch]]++;
+			res[img.at<cv::Vec3b>(i, j)[ch]]++;
 		}
 	return res;
 }
@@ -27,7 +28,7 @@ int main(int argc, char** argv)
 		std::cerr << "Image name is needed\n";
 		return -1;
 	}
-	cv::Mat image = cv::imread(argv[1]);
+	cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
 	if(!image.data)
 	{
 		std::cerr << "Cant read image\n";
@@ -35,27 +36,30 @@ int main(int argc, char** argv)
 	}
 	std::cout << "Image is read\n";
 	//Calculatioing histograms
-	std::vector<std::vector<long long>> hist = {calcHist(image, 1), calcHist(image, 2), calcHist(image, 3)};
-	//Sorting histograms
-	std::vector<std::multimap<long long, int, std::greater<long long>>> shist; //count - color
+	std::vector<std::vector<long long>> hist = {calcHist(image, 0), calcHist(image, 1), calcHist(image, 2)};
+	std::cout << "Hists are done\n";
+	for(int ch = 0; ch < 3; ++ch)
+	{
+		for(auto v: hist[ch])
+			std::cout << v << " ";
+		std::cout << "\n";
+	}
+	//Choosing the peaks
+	long long s = image.rows * image.cols;
+	std::vector<std::set<int>> peaks;
 	for(auto h: hist)
 	{
-		shist.push_back(std::multimap<long long, int, std::greater<long long>>());
-		for(int i = 0; i < h.size(); ++i)
-			shist.back().insert(std::make_pair(h[i], i));
-	}
-	//Choosing the biggest
-	std::vector<std::set<int>> biggest;
-	for(auto h: shist)
-	{
-		biggest.push_back(std::set<int>());
-		int count = 0;
-		for(auto i = h.begin(); i != h.end() && count < COUNT_OF_THE_BIGGEST; ++i, ++count)
+		peaks.push_back(std::set<int>());
+		for(int i = 0; i < 256; ++i)
 		{
-			biggest.back().insert(i->second);
-		}	
+			if((i-distance > 0 && h[i-distance] < h[i] - s*diff_factor) || 
+			   (i+distance < 256 && h[i+distance] < h[i] - s*diff_factor))
+			{
+				peaks.back().insert(i);
+			}
+		}
 	}
-	for(auto b: biggest)
+	for(auto b: peaks)
 	{
 		for(auto color_ch: b)
 			std::cout << color_ch << " ";
